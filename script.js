52f10f780125de905eda1a9a45b9a6de6496c907
lonqpham <longp11111@gmail.com>
2024-05-30 17:44:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 }
+
+function showRequestForm() {
+  document.getElementById('request-popup').style.display = 'block';
+}
+
+function hideRequestForm() {
+  document.getElementById('request-popup').style.display = 'none';
+}
